42c6d107b005fefa81e57eeaafd5e2e1cf413994
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2013-02-04 21:52:49 +0000

    NUEVO CONSTRUCTOR
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/jsfmatrix2@8669 13f10598-5c34-4a87-bf25-6f6a81c8df7a

--- src/main/java/com/gather/jsfmatrix/core/view/DefaultWidgetView.java
+++ src/main/java/com/gather/jsfmatrix/core/view/DefaultWidgetView.java
@@ -1,10 +1,15 @@
 package com.gather.jsfmatrix.core.view;
 
 import com.gather.jsfmatrix.core.app.uicomponent.DirectoryUIWidget;
+import com.gather.jsfmatrix.core.view.uiobject.UIJSFObject;
 
 public class DefaultWidgetView extends DefaultJSFView {
 
     public DefaultWidgetView() {
         super(new DirectoryUIWidget());
+    }
+
+    public DefaultWidgetView(UIJSFObject uijsfObject) {
+        super(uijsfObject);
     }
 }
